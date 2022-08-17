88c08d471c620b7f325359ff50a5251e7fd7a031
Nikolas <nkls99@gmail.com>
2022-08-17 14:18:15 -0300

    another one

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,11 @@ import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
+const allowed = ["https://bar-tab-front-end.vercel.app", "http://localhost:3000", "https://bar-tab-front-end.vercel.app/", "https://bar-tab-front-end-nikolasrr.vercel.app"]
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://bar-tab-front-end.vercel.app"] }));
+app.use(cors({ credentials: true, origin: allowed }));
 app.use(router);
 app.use(errorHandler);
 
